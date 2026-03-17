f7289a8c6a160afb26805a1065c181cdfab1297a
Davidzambrano16 <david-z-p@hotmail.com>
2026-03-16 22:29:02 -0400

    Fix case sensitivity in imports

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 export * from './usuarioControllers.js';
-export * from './disciplinaController.js';
+export * from './disciplinaControllers.js';
 export * from './equiposControllers.js';
 export * from './torneoControllers.js';
 export * from './partidoControllers.js';
